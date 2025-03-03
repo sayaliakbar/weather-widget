6ab63359ab6b97e1a9c6eab4af3a13293e711e8c
Ali Akbar <sayaliakbar@gmail.com>
2025-03-04 04:47:47 +0500

    Refactor App component: remove WeatherApp and import App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import WeatherApp from "./WeatherApp";
+import "./App.css";
 
 function App() {
-  return (
-    <div>
-      <WeatherApp></WeatherApp>
-    </div>
-  );
+  return <></>;
 }
 
 export default App;
