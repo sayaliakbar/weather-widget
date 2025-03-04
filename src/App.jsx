ff6485c36595e17f9557769d1cf054351742fa67
Ali Akbar <sayaliakbar@gmail.com>
2025-03-04 07:17:42 +0500

    Add weather data fetching and display functionality to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,42 @@
 import "./App.css";
+import Mainweather from "./components/Mainweather";
 import Navbar from "./components/Navbar";
+import { useState, useEffect } from "react";
 
 function App() {
+  const apiUrl = import.meta.env.VITE_API_URL;
+  const apiKey = import.meta.env.VITE_API_KEY;
+
+  const [city, setCity] = useState("quetta");
+  const [weatherData, setWeathterData] = useState(null);
+
+  useEffect(() => {
+    fetchWeatherData();
+  }, [city]);
+
+  const fetchWeatherData = () => {
+    fetch(`${apiUrl}?q=${city}&units=metric&appid=${apiKey}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setWeathterData(data);
+      });
+  };
+
+  const handleSearch = (searchedCity) => {
+    setCity(searchedCity);
+  };
+
   return (
-    <>
-      <Navbar />
-    </>
+    <div>
+      <Navbar onSearch={handleSearch} />
+      {weatherData && (
+        <div style={{ display: "flex", padding: "30px", gap: "20px" }}>
+          <div style={{ flex: "1", marginRight: "10px" }}>
+            <Mainweather weatherData={weatherData} />
+          </div>
+        </div>
+      )}
+    </div>
   );
 }
 
