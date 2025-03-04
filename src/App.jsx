4139b8e1ea822f58fb77b7d311aa1bebe1bf8a89
Ali Akbar <sayaliakbar@gmail.com>
2025-03-04 20:08:26 +0500

    Add air quality data fetching and display in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,40 @@
 import "./App.css";
 import Mainweather from "./components/Mainweather";
 import Navbar from "./components/Navbar";
+import TodayHighlights from "./components/TodayHighlights";
 import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
   const apiUrl = import.meta.env.VITE_API_URL;
   const apiKey = import.meta.env.VITE_API_KEY;
 
   const [city, setCity] = useState("quetta");
-  const [weatherData, setWeathterData] = useState(null);
+  const [weatherData, setWeathterData] = useState("quetta");
+  const [airQualityData, setAirQualityData] = useState(null);
+  const [fiveDayForecast, setFiveDayForecast] = useState(null);
 
   useEffect(() => {
     fetchWeatherData();
   }, [city]);
 
+  const fetchAirQualityData = (lat, lon) => {
+    axios
+      .get(`${apiUrl}/air_pollution?lat=${lat}&lon=${lon}&appid=${apiKey}`)
+      .then((response) => {
+        setAirQualityData(response.data.list[0]);
+      })
+      .catch((error) =>
+        console.log("Error fetching the air quality data:", error)
+      );
+  };
+
   const fetchWeatherData = () => {
-    fetch(`${apiUrl}?q=${city}&units=metric&appid=${apiKey}`)
-      .then((response) => response.json())
-      .then((data) => {
-        setWeathterData(data);
+    axios
+      .get(`${apiUrl}/weather?q=${city}&units=metric&appid=${apiKey}`)
+      .then((response) => {
+        setWeathterData(response.data);
+        fetchAirQualityData(response.data.coord.lat, response.data.coord.lon);
       });
   };
 
@@ -31,8 +47,21 @@ function App() {
       <Navbar onSearch={handleSearch} />
       {weatherData && (
         <div style={{ display: "flex", padding: "30px", gap: "20px" }}>
-          <div style={{ flex: "1", marginRight: "10px" }}>
+          <div style={{ marginRight: "10px" }}>
             <Mainweather weatherData={weatherData} />
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              flex: "1",
+              gap: "20px",
+            }}
+          >
+            <TodayHighlights
+              weatherData={weatherData}
+              airQualityData={airQualityData}
+            />
           </div>
         </div>
       )}
