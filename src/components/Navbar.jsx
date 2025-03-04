d030b5aa4f2547899ebd0848f9f67fd4a75ea4a4
Ali Akbar <sayaliakbar@gmail.com>
2025-03-05 03:06:44 +0500

    Refactor Navbar component to utilize current city for search and update header to 'Weather Widget'

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,11 @@
 import React from "react";
 import { FilterDrama, GpsFixed } from "@mui/icons-material";
 import { useState } from "react";
-import axios from "axios";
 
-// import GpsFixedIcon from "@mui/icons-material/GpsFixed";
 import { TextField, Button } from "@mui/material";
 
-const Navbar = ({ onSearch }) => {
+const Navbar = ({ onSearch, currentCity }) => {
   const [searchCity, setSearchCity] = useState("");
-
-  const apiURL = import.meta.env.VITE_API_URL;
-  const apiKey = import.meta.env.VITE_API_KEY;
 
   const handleSearchCity = () => {
     if (searchCity.trim()) {
@@ -20,25 +15,8 @@ const Navbar = ({ onSearch }) => {
   };
 
   const handleCurrentCity = () => {
-    if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const { latitude, longitude } = position.coords;
-          axios
-            .get(
-              `${apiURL}/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`
-            )
-            .then((response) => {
-              onSearch(response.data.name);
-            });
-        },
-        (error) => {
-          console.error("Error retrieving location:", error);
-        }
-      );
-    } else {
-      console.log("Geolocation is not available in this browser.");
-    }
+    console.log(currentCity);
+    onSearch(currentCity);
   };
   return (
     <nav
@@ -54,7 +32,7 @@ const Navbar = ({ onSearch }) => {
     >
       <div style={{ display: "flex", alignItems: "center", gap: "2px" }}>
         <FilterDrama />
-        <p style={{ fontWeight: "bold", fontSize: "20px" }}>Weather App</p>
+        <h1>Weather Widget</h1>
       </div>
       <div style={{ display: "flex", alignItems: "center", gap: "4px" }}>
         <TextField
