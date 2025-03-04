94166eff9178070e02cd4fef35a939bdf904d024
Ali Akbar <sayaliakbar@gmail.com>
2025-03-04 20:33:18 +0500

    Refactor HighlightBox component to accept props for title, value, and icon, enhancing its display and styling

--- src/components/HighlightBox.jsx
+++ src/components/HighlightBox.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
-const HighlightBox = () => {
-  return <div>HighlightBox</div>;
+const HighlightBox = ({ title, value, icon }) => {
+  return (
+    <div
+      style={{
+        backgroundColor: "#374151",
+        color: "white",
+        padding: "1rem",
+        borderRadius: "0.5rem",
+        marginTop: "11px",
+        flex: "1",
+      }}
+    >
+      <p>{title}</p>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          fontSize: "2rem",
+          alignItems: "center",
+        }}
+      >
+        {icon}
+        {value}
+      </div>
+    </div>
+  );
 };
 
 export default HighlightBox;
