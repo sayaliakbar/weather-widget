a15409d263030dafe0b0ae3be9034555d6b91fda
Ali Akbar <sayaliakbar@gmail.com>
2025-03-05 05:11:10 +0500

    Refactor Navbar component to use MUI AppBar and improve search functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,72 +1,107 @@
-import React from "react";
+import * as React from "react";
+import { styled, alpha } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
 import { FilterDrama, GpsFixed } from "@mui/icons-material";
+import { InputBase } from "@mui/material";
 import { useState } from "react";
 
-import { TextField, Button } from "@mui/material";
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(3),
+    width: "auto",
+  },
+}));
 
-const Navbar = ({ onSearch, currentCity }) => {
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
+    },
+  },
+}));
+
+const Navbar = ({ onSearch }) => {
   const [searchCity, setSearchCity] = useState("");
 
-  const handleSearchCity = () => {
+  const handleSearchCity = (e) => {
+    e.preventDefault();
     if (searchCity.trim()) {
       onSearch(searchCity);
       setSearchCity("");
     }
   };
 
-  const handleCurrentCity = () => {
-    console.log(currentCity);
-    onSearch(currentCity);
-  };
   return (
-    <nav
-      style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "space-between",
-        marginTop: "10px",
-        paddingTop: "10px",
-        paddingLeft: "30px",
-        padddingRight: "30px",
-      }}
-    >
-      <div style={{ display: "flex", alignItems: "center", gap: "2px" }}>
-        <FilterDrama />
-        <h1>Weather Widget</h1>
-      </div>
-      <div style={{ display: "flex", alignItems: "center", gap: "4px" }}>
-        <TextField
-          style={{
-            backgroundColor: "white",
-            borderRadius: "2rem",
-            maxWidth: "22rem",
-          }}
-          variant="outlined"
-          placeholder="Search city"
-          value={searchCity}
-          onChange={(e) => setSearchCity(e.target.value)}
-        />
-        <Button
-          style={{ borderRadius: "6px", backgroundColor: "#4B5550" }}
-          variant="contained"
-          onClick={handleSearchCity}
-        >
-          Search
-        </Button>
-      </div>
-      <Button
-        onClick={handleCurrentCity}
-        variant="contained "
-        style={{
-          borderRadius: "6px",
-          backgroundColor: "#4B5550",
-          color: "white",
-        }}
-      >
-        <GpsFixed />
-        Current Location
-      </Button>
-    </nav>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" sx={{ backgroundColor: "#4B5563" }}>
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+          >
+            <FilterDrama />
+          </IconButton>
+
+          <Typography
+            noWrap
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+          >
+            Weather Widget
+          </Typography>
+          <form style={{ display: "flex" }} onSubmit={handleSearchCity}>
+            <Search>
+              <SearchIconWrapper>
+                <SearchIcon />
+              </SearchIconWrapper>
+              <StyledInputBase
+                placeholder="Search city…"
+                inputProps={{ "aria-label": "search" }}
+                value={searchCity}
+                onChange={(e) => setSearchCity(e.target.value)}
+              />
+            </Search>
+            <Button type="submit" color="inherit" variant="outlined">
+              Search
+            </Button>
+          </form>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 };
 
