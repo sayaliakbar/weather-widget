9e2173edca0603598e42e4a6e73330b8d9f907f2
Ali Akbar <sayaliakbar@gmail.com>
2025-03-05 08:06:02 +0500

    Refactor Mainweather component to improve layout and styling consistency

--- src/components/Mainweather.jsx
+++ src/components/Mainweather.jsx
@@ -43,42 +43,30 @@ const Mainweather = ({ weatherData }) => {
   };
 
   return (
-    <div
-      style={{
-        backgroundColor: "#4B5563",
-        color: "white",
-        borderRadius: "0.5rem",
-        width: "160px",
-        padding: "30px",
-      }}
-    >
+    <>
       <div style={{ fontSize: "20px" }}>Now</div>
       <div
         style={{
           display: "flex",
           alignItems: "center",
-          fontSize: "35px",
+          fontSize: "2rem",
           fontWeight: "bold",
         }}
       >
         {temperatureCelcius}°C{renderTemperateIcon()}
       </div>
-      <div style={{ fontSize: "15px", marginTop: "8px", fontWeight: "50px" }}>
-        {weatherDescription}
-      </div>
+      <div>{weatherDescription}</div>
       <div style={{ marginTop: "1rem" }}>
         <div style={{ display: "flex", alignItems: "center" }}>
           <CalendarMonth />
           {currentDate}
         </div>
-        <div
-          style={{ marginTop: "4px", display: "flex", alignItems: "center" }}
-        >
+        <div style={{ display: "flex", alignItems: "center" }}>
           <LocationOn />
-          {cityName},{countryName}
+          {cityName} {countryName}
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
